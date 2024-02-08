e66ca807f9d662de470621e6c41047f864eee014
Eren <erenbas3453@gmail.com>
2024-02-08 16:52:48 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Concrete;
+using System;
+
+
+namespace Workaround 
+{
+    class Program 
+    {
+
+        static void Main(string[] args) { 
+            Person person = new Person();
+
+            SelamVer("Eren");
+
+            int toplam = Topla(2, 5);
+        }
+
+        static void SelamVer(string isim = "Nameless") {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla(int sayi1 = 0, int sayi2 = 0) {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam : " + sonuc.ToString());
+            
+            return sonuc;
+        }
+    }
+
+    public class Vatandas {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long tcNo { get; set; }
+    }
+}
